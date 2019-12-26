5d0493b6df6641c9f5120c315c9987f9140d71e3
Kiruthiga21 <kirucse97@gmail.com>
2019-12-26 15:19:52 +0530

    Updated

--- src/main/java/Cluase/SantaCluase.java
+++ src/main/java/Cluase/SantaCluase.java
@@ -16,5 +16,12 @@ public class SantaCluase {
 
 			System.out.println();
 		}
+		System.out.println(1);
+		System.out.println(2);
+		System.out.println(3);
+		System.out.println(4);
+		System.out.println(5);
+		System.out.println(6);
+		System.out.println(7);
 	}
 }
