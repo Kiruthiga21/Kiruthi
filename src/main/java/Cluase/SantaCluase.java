56514ec8d36c3c6825853e9a9e2686b980d5d2a4
Kiruthiga21 <kirucse97@gmail.com>
2019-12-26 15:28:03 +0530

    new updates

--- src/main/java/Cluase/SantaCluase.java
+++ src/main/java/Cluase/SantaCluase.java
@@ -5,6 +5,13 @@ public class SantaCluase {
 		int n = 7;
 		int ans = 97;
 		char res = (char) ans;
+		System.out.println(1);
+		System.out.println(2);
+		System.out.println(3);
+		System.out.println(4);
+		System.out.println(5);
+		System.out.println(6);
+		System.out.println(7);
 		for (int i = 1; i <= n; i++) {
 			for (int j = 1; j <= n; j++) {
 				if (j == i || i + j == n + 1) {
